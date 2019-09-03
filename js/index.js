76dfae1a1e821168c9883d4c4734ac895c2db9a2
askart <tulebaev.askar@gmail.com>
2019-09-03 17:13:34 +0600

    put modules inside slider

--- js/index.js
+++ js/index.js
@@ -1,9 +1,30 @@
 (function() {
-  new Glide('.glide', {
+  new Glide('.section1', {
     type: 'carousel',
     startAt: 0,
     gap: 0,
     autoplay: 10000,
     animationDuration: 1000,
+  }).mount();
+  new Glide('.modules', {
+    type: 'carousel',
+    perView: 5,
+    gap: 50,
+    breakpoints: {
+      1700: {
+        perView: 4,
+      },
+      1400: {
+        perView: 3,
+      },
+      1100: {
+        perView: 2,
+        gap: 25,
+      },
+      800: {
+        perView: 1,
+        gap: 0,
+      },
+    },
   }).mount();
 })();
